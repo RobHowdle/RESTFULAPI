a3b9dc5c2f358320b34b07b335dc66f9d5d43c82
Rob Howdle <robhowdle94@gmail.com>
2020-09-18 13:57:47 +0100

    Fixed Post Constructor Errors

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const PostSchema = mongoose.Schema({
+const PostSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true
@@ -15,4 +15,4 @@ const PostSchema = mongoose.Schema({
     }
 });
 
-module.export = mongoose.model('Posts', PostSchema);
+module.exports = mongoose.model('Posts', PostSchema);
